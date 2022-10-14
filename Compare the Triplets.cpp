c9d53f380e0271c509c0dffc9769f078ec3b9742
sshreyash18 <109620135+sshreyash18@users.noreply.github.com>
2022-10-14 19:11:20 +0530

    Add files via upload

---
+++ Compare the Triplets.cpp
@@ -0,0 +1,58 @@
+/* Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+
+If a[i] > b[i], then Alice is awarded 1 point.
+If a[i] < b[i], then Bob is awarded 1 point.
+If a[i] = b[i], then neither person receives a point.
+Comparison points is the total points a person earned.
+
+Given a and b, determine their respective comparison points.
+
+Example
+
+a = [1, 2, 3]
+b = [3, 2, 1]
+For elements *0*, Bob is awarded a point because a[0] .
+For the equal elements a[1] and b[1], no points are earned.
+Finally, for elements 2, a[2] > b[2] so Alice receives a point.
+The return array is [1, 1] with Alice's score first and Bob's second. */
+
+
+
+
+
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int alice =0,bob=0;
+    int arr1[3];
+    int arr2[3];
+    for(int i=0;i<3;i++){
+        cin>>arr1[i];
+    }
+    for(int i=0;i<3;i++){
+        cin>>arr2[i];
+    }
+    for(int j=0;j<3;j++){
+        if(arr1[j]>arr2[j]){
+           alice+=1; 
+        }
+        else if(arr1[j]<arr2[j]){
+            bob+=1;
+        }
+        else{
+            alice+=0;
+            bob+=0;
+        }
+        
+    }
+    cout<<alice<<" "<<bob;
+    
+}
